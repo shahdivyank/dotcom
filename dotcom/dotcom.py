9750ec1e1c66ce7f0c0cfbcc6306f7b502aeb4ea
Andrew Khadder <andrewkh15@gmail.com>
2024-04-04 13:42:40 -0700

    format + gitignore fix

--- dotcom/dotcom.py
+++ dotcom/dotcom.py
@@ -8,13 +8,17 @@ from inspect import signature
 
 BASE_FILEPATH = "./api"
 
+
 class Dotcom:
 
     def __init__(self):
         load_dotenv()
 
     def _parse_query(self, query):
-        results = re.findall("^[a-zA-Z0-9_-]+=[a-zA-Z0-9%._-]+(&[a-zA-Z0-9_-]+=[a-zA-Z0-9%._-]+)*$", query)
+        results = re.findall(
+            "^[a-zA-Z0-9_-]+=[a-zA-Z0-9%._-]+(&[a-zA-Z0-9_-]+=[a-zA-Z0-9%._-]+)*$",
+            query,
+        )
 
         if results == []:
             return {}
@@ -25,14 +29,14 @@ class Dotcom:
         for param in params:
             [key, value] = param.split("=")
             queryParams[key] = value
-        
+
         return queryParams
 
     def _parse_params(self, route, filepath):
         params = dict()
 
         split_filepath = filepath.split("/")
-        split_route  = route.split("/")
+        split_route = route.split("/")
 
         for index in range(len(split_filepath)):
             if split_filepath[index] != split_route[index]:
@@ -41,11 +45,11 @@ class Dotcom:
         return params
 
     async def _execute(self, module, method, receive, query, params):
-        req = { "query": query, "params": params }
+        req = {"query": query, "params": params}
 
         if method == "POST":
-            body  = await receive()
-            body = body["body"].decode('utf-8')
+            body = await receive()
+            body = body["body"].decode("utf-8")
             req["body"] = json.loads(body)
 
         function = getattr(module, method)
@@ -59,33 +63,41 @@ class Dotcom:
     async def run(self, scope, receive, send):
         method = scope["method"]
         path = scope["path"]
-        query = self._parse_query(scope["query_string"].decode('utf-8'))
+        query = self._parse_query(scope["query_string"].decode("utf-8"))
 
         response = ""
 
-        all_possible_routes = glob(os.path.join(BASE_FILEPATH, "**", r"route.py"), recursive=True)
+        all_possible_routes = glob(
+            os.path.join(BASE_FILEPATH, "**", r"route.py"), recursive=True
+        )
 
         for possible_route in all_possible_routes:
             trimmed_route = possible_route[5:-9]
-            striped_route = re.sub("\[[A-Za-z0-9_-]+\]", "[A-Za-z0-9_-]+", trimmed_route)
+            striped_route = re.sub(
+                "\[[A-Za-z0-9_-]+\]", "[A-Za-z0-9_-]+", trimmed_route
+            )
             striped_route = "^" + striped_route + "$"
             search = re.match(striped_route, path)
-            
+
             if search:
                 params = self._parse_params(path, trimmed_route)
                 route = possible_route.replace("/", ".").replace(".py", "")[2:]
                 module = import_module(route)
                 response = await self._execute(module, method, receive, query, params)
 
-        await send({
-        'type': 'http.response.start',
-        'status': 200,
-        'headers': [
-            [b'content-type', b'text/plain'],
-        ],
-        })
+        await send(
+            {
+                "type": "http.response.start",
+                "status": 200,
+                "headers": [
+                    [b"content-type", b"text/plain"],
+                ],
+            }
+        )
 
-        await send({
-            'type': 'http.response.body',
-            'body': response.encode('utf-8'),
-        })
+        await send(
+            {
+                "type": "http.response.body",
+                "body": response.encode("utf-8"),
+            }
+        )
